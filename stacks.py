6b578adf8935a67bd8a6cd52c1a9cd99787239de
Eusebio <a00397797@itesm.mx>
2017-04-09 14:41:30 -0500

    stacks
    se agregaron funciones para facilitar el manejo de stacks así como para
    ver el último elemento y saber si están dentro del mismo orden.

--- stacks.py
+++ stacks.py
@@ -1,8 +1,65 @@
+from operators import operators
+
 fStack = []
 vStack = []
 dCounter = 0
 
+operandsStack = [] #operands stack for quadruples
+operatorsStack = []#operators stack for quadruples
+typesStack = [] #types stack for quadruples
 
+#temporal counter. only for testing purposes
+temporalCounter = 1
+
+FALSE_BOTTOM = '?'
+
+#function to lookup the last element on operator stack
+def lookOperatorStack():
+    if len(operatorsStack) == 0:
+        return None
+    if operatorsStack[ len(operatorsStack) - 1] == FALSE_BOTTOM:
+        return None
+    return operatorsStack[ len(operatorsStack) - 1]
+
+#checks if the last operator on stack is the given operator
+def isLastOperator(operator):
+    if lookOperatorStack() is None:
+        return False
+    return lookOperatorStack() == operators[operator]
+
+#checks if the last operator on stack is in the same order
+def isSameOrder(operator):
+    if lookOperatorStack() is None:
+        return False
+    if operator == '+' or operator == '-':
+        return isLastOperator('+') or isLastOperator('-')
+    if operator == '*' or operator == '/':
+        return isLastOperator('*') or isLastOperator('/')
+    if operator == '==' or operator == '>' or operator == '<':
+        return isLastOperator('==') or isLastOperator('>') or isLastOperator('<')
+    if operator == '&' or operator == '|':
+        return isLastOperator('&') or isLastOperator('|') 
+    return None #error. Bad operator given
+
+def getLastOperand():
+    if len(operandsStack) < 1:
+        return None
+    return operandsStack[ len(operandsStack) - 1]
+
+def getPenultimateOperand():
+    if len(operandsStack) < 2:
+        return None
+    return operandsStack[ len(operandsStack) - 2]
+
+def getLastType():
+    if len(typesStack) < 1:
+        return None
+    return typesStack[ len(typesStack) - 1]
+
+def getPenultimateType():
+    if len(typesStack) < 2:
+        return None
+    return typesStack[ len(typesStack) - 2]
 
 def getLastVariable():
     return vStack[len(vStack) - 1]

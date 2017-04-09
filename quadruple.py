68c18f886227e1d10bb11bf1b87f3e871b900d90
Eusebio <a00397797@itesm.mx>
2017-04-08 21:14:35 -0500

    cuádruplo
    primera propuesta básica de cuádruplo

---
+++ quadruple.py
@@ -0,0 +1,12 @@
+class QuadrupleManager:
+
+    def __init__(self):
+        self.quadrupleStack = []
+
+class Quadruple :
+
+    def __init__(self):
+        self.operator = 0;
+        self.first_operand = 0;
+        self.first_operand = 0;
+        self.result = 0;

f9eb8ddfabae121f02bb2c6c83d0884c71e66db0
Eusebio <a00397797@itesm.mx>
2017-04-09 14:42:18 -0500

    cuádruplo
    implementación básica del cuádruplo así como un método para agregarla
    usando id de manera temporal. Falta cambiar id por memoria

--- quadruple.py
+++ quadruple.py
@@ -3,10 +3,15 @@ class QuadrupleManager:
     def __init__(self):
         self.quadrupleStack = []
 
+    #adds quadruple to stack
+    def addQuadruple (self, operator, operand1, operand2, result):
+        self.quadrupleStack.append(Quadruple(operator, operand1, operand2, result))
+        return result
+
 class Quadruple :
 
-    def __init__(self):
-        self.operator = 0;
-        self.first_operand = 0;
-        self.first_operand = 0;
-        self.result = 0;
+    def __init__(self, operator, operand1, operand2, result):
+        self.operator = operator;
+        self.firstOperand = operand1;
+        self.secondOperand = operand2;
+        self.result = result;

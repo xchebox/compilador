01f609ebdab501d7d9a98964d8ad0cfa37c552d9
Eusebio <a00397797@itesm.mx>
2017-04-24 21:01:15 -0500

    arreglo
    cálculo de dimensiones y variables ‘m’

--- variable_table.py
+++ variable_table.py
@@ -18,6 +18,11 @@ class VarTable:
     def getVariable(self, id):
         return self.table[id]
 
+class Dimension:
+    def __init__(self, size):
+        self.size = size
+        self.m = 0
+
 class Variable:
 
     def __init__(self, var_memory, var_type):
@@ -38,15 +43,26 @@ class Variable:
         self.type = var_type;
 
     def addDimension(self, newDimension):
-        self.dimension.append(newDimension)
+        self.dimension.append(Dimension(newDimension))
 
+        #calculates m's and return the total size of array
     def getTotalMemoryDimension(self):
         if len (self.dimension) == 0:
             return 1
-        if len (self.dimension ) == 1:
-            return self.dimension[0]
-        totalMemory = reduce( lambda x, y: x*y, self.dimension)
-        return totalMemory
+        if len (self.dimension) == 1:
+            return self.dimension[0].size
+        totalMemory = 1
+        for d in self.dimension:
+            totalMemory *= d.size
+            #at this point we have the total memory
+        for d in self.dimension:
+            d.m = totalMemory/d.size#calculates all m's
+        self.dimension[len(self.dimension) - 1].m = 0 #last dimension m's 0
+
+        for d in self.dimension:
+            print d.m
+
+        return  self.dimension[0].m * self.dimension[0].size #Total size
 
     def getDimension(self):
         return self.dimension
